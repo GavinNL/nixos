9bb6c290b4e2f12e6fdc47e399d745a6017a5e76
Gavin <gavin.n.lobo@gmail.com>
2025-06-15 21:36:03 -0400

    added niri

---
+++ modules/desktop_niri.nix
@@ -0,0 +1,20 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, inputs, pkgs, ... }:
+
+{
+  programs.hyprland.enable = true;
+
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    niri
+    
+  ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+}
